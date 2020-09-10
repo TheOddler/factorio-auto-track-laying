61fc85ac68debf93bf9367eca742cf759209bffa
Pablo <pablo.bollansee@gmail.com>
2020-09-10 23:25:32 +0200

    ✨ New way of calculating the build area

--- control.lua
+++ control.lua
@@ -1,4 +1,10 @@
-local radius = 2
+local radius_player = 1.8
+local radius_vehicle = 3
+
+function sign(x)
+    if (x < 0) then return -1 end
+    return 1
+end
 
 local function can_build(item, player)
     return string.find(item.name, "rail") -- Player can only build rail-related items
@@ -30,17 +36,56 @@ local function try_build_entity(entity, player)
     end
 end
 
+local function calculate_length(bounding_box)
+    local bb_lt = bounding_box.left_top
+    local bb_rb = bounding_box.right_bottom
+    return math.max(bb_rb.x - bb_lt.x, bb_rb.y - bb_lt.y)
+end
+
 local function get_build_area(player)
-    local x = player.position.x
-    local y = player.position.y
-    return {
-        {x - radius, y - radius},
-        {x + radius, y + radius}
+    local entity = player.vehicle or player.character
+
+    -- Get some basic values first
+    local speed_sign = sign(entity.speed or entity.character_running_speed) -- moving forward or backwards?
+    local x = entity.position.x
+    local y = entity.position.y
+
+    -- Then calculate some values
+    local rad = entity.orientation * 2 * math.pi
+    local dir_x = math.sin(rad)
+    local dir_y = -math.cos(rad)
+    local length = calculate_length(entity.bounding_box)
+    local radius = player.vehicle and radius_vehicle or radius_player -- kinda like ternary
+
+    -- Offset the position
+    local x = x + dir_x * length * speed_sign
+    local y = y + dir_y * length * speed_sign
+
+    -- Create an area around the position
+    local area = {
+        left_top = { x - radius, y - radius },
+        right_bottom = { x + radius, y + radius }
     }
+    
+    -- Debug draw
+    -- rendering.draw_rectangle{
+    --     color = {r = 1},
+    --     width = 2,
+    --     filled = false,
+    --     left_top = area.left_top,
+    --     right_bottom = area.right_bottom,
+    --     surface = 1,
+    --     time_to_live = 2
+    -- }
+
+    return area
 end
 
 local function on_player_changed_position(event)
     local player = game.get_player(event.player_index)
+
+    if (not player.character) then return end
+
     local entities = player.surface.find_entities(get_build_area(player))
     
     for _, entity in pairs(entities) do
