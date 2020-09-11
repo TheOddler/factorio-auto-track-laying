c84b5772e74ba906fb7f726dfac074951beb19ad
Pablo <pablo.bollansee@gmail.com>
2020-09-11 16:18:33 +0200

    ✨ Use point and radius rather than area

--- control.lua
+++ control.lua
@@ -46,9 +46,7 @@ local function calculate_direction(entity)
     }
 end
 
-local function get_build_area(player)
-    local entity = player.vehicle or player.character
-
+local function get_point_in_front_of(entity)
     -- Get some basic values first
     local speed_sign = sign(entity.speed or entity.character_running_speed) -- moving forward or backwards?
     local x = entity.position.x
@@ -58,43 +56,42 @@ local function get_build_area(player)
     local rad = entity.orientation * 2 * math.pi
     local dir = calculate_direction(entity)
     local length = calculate_length(entity.bounding_box)
-    local radius = player.vehicle and radius_vehicle or radius_player -- kinda like ternary
 
     -- Offset the position
     local x = x + dir.x * length * speed_sign
     local y = y + dir.y * length * speed_sign
 
-    -- Create an area around the position
-    local area = {
-        left_top = { x - radius, y - radius },
-        right_bottom = { x + radius, y + radius }
+    return {
+        x = x,
+        y = y
     }
-
-    return area
 end
 
 local function on_player_changed_position(event)
     local player = game.get_player(event.player_index)
+    local entity = player.vehicle or player.character
 
-    if (not player.character) then return end
+    if (not entity) then return end
 
-    local area = get_build_area(player)
+    local radius = player.vehicle and radius_vehicle or radius_player -- kinda like ternary
+    local position = get_point_in_front_of(entity)
+
     local entities = player.surface.find_entities_filtered{
-        area = area,
-        -- position = …, radius = …,
+        position = position,
+        radius = radius,
         type = "entity-ghost"
     }
     
     -- Debug draw
-    rendering.draw_rectangle{
-        color = {r = 1},
-        width = 2,
-        filled = false,
-        left_top = area.left_top,
-        right_bottom = area.right_bottom,
-        surface = 1,
-        time_to_live = 2
-    }
+    -- rendering.draw_circle{
+    --     color = {r = 1},
+    --     width = 2,
+    --     filled = false,
+    --     target = position,
+    --     radius = radius,
+    --     surface = 1,
+    --     time_to_live = 2
+    -- }
     
     for _, entity in pairs(entities) do
         if entity.valid then
